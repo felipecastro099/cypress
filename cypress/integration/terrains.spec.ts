d8474e77cd41f38e5bd5601bee403c82d6bc33e3
Felipe Castro <me@felipecastro.dev>
2022-04-30 19:53:24 -0300

    create terrain test

--- cypress/integration/terrains.spec.ts
+++ cypress/integration/terrains.spec.ts
@@ -7,6 +7,12 @@ describe('cities test', () => {
     })
 
     it('create a new terrain', () => {
+        cy.visit('http://localhost:10000/atlas/geopolygon/')
+
+        cy.contains('Adicionar').click()
+
+        cy.contains('Salvar').click()
+
         cy.visit('http://localhost:10000/greenfield/terrain')
 
         cy.contains('Adicionar').click()
@@ -15,7 +21,8 @@ describe('cities test', () => {
         cy.get('input[name="name"]').type("name").should('have.value','name')
         cy.get('[id=select2-id_city-container]').click()
         cy.get('input[class=select2-search__field]').type("name").should('have.value','name').type('{enter}')
-        cy.get('[id=select2-id_status-container]').click()
+        cy.get('[id=select2-id_polygon-container]').click()
+        cy.get('input[class=select2-search__field]').type("po").should('have.value','po').type('{enter}')
 
         cy.contains('Salvar').click()
 
